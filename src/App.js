c634a67de37ff06395b9e58c61301f7d4eba43db
ForbesR86 <Randolph.forbes@gmail.com>
2021-11-06 18:42:05 -0400

    initial creation of pages
    initial creation of pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Switch, Route, Router} from "react-router-dom";
+import { Container, Row, Col } from 'react-bootstrap';
+import axios from 'axios';
+import { createBrowserHistory } from "history";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const history = createBrowserHistory();
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      localToken: localStorage.token,
+      token:[],
+      user: [],
+      currentUser: [],
+      currentUserID: "",
+      registeredUser: [],
+      loggedIn: false,
+      books:[],
+      searchResults: [],
+      searchEnable: false,
+      usertype:false,
+    };
+
+  };
+
+  render() {
+    return (
+      <Container fluid>
+
+        <Row>
+          <Col>Header Here</Col>
+        </Row>
+
+        <Row>
+          <Col sm={8}>Menuu Links</Col>
+          <Col sm={4}>Login/Logout/User Info</Col>
+        </Row>
+
+        <Row>
+          <Col>Main body here</Col>
+        </Row>
+
+        <Row>
+          <Col>Footer</Col>
+        </Row>
+
+      </Container>
   );
+  };
 }
-
+ 
 export default App;
